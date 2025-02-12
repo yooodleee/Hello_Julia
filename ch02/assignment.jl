54b0d96d6500920ec75ac0a28720f318af7c346f
yooodleee <accia25@naver.com>
2025-02-12 12:20:10 +0900

    code

---
+++ ch02/assignment.jl
@@ -0,0 +1,16 @@
+# 할당문(assignment statement)
+
+message = "And now for something compeletly different"
+# "And now for something compeletly different
+
+n = 17
+# 17
+
+pi_val = 3.141592653589793
+# 3.141592653589793
+
+"""
+첫 번째는 문자열을 message라는 변수에 할당했고
+두 번째는 정수 17을 n에,
+세 번째는 원주율 π의 값을 π_val에 할당했다(\pi + tab)
+"""
